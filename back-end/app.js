8cdf2daa108422b7179c86bcd8780bcdceaf4098
Jarawee THONGCHAROEN <jarawee.t@ku.th>
2020-10-17 02:29:41 +0700

    [+] Access-Control-Allow-Headers

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,22 @@
 import express from "express";
 import routeHanlder from './routers';
 import passport from './utils/passport';
+import cors from 'cors';
 
 const app = express();
 
 require("dotenv").config();
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 
 app.use(passport.initialize());
 
